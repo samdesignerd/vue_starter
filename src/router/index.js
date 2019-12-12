43e4db46b05d758313935a8d8b3d5aad2b1c47f2
samdesignerd <sam@boulanger.design>
2019-12-12 10:29:59 +0100

    Changing names : component becoming widget in file tree

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,14 @@ let routes = [
 export default new Router({
   routes: routes.map((route)=>{
     if (route.component) {
-      route.component = require(`../components/pages/${route.component}/p-layout.vue`).default
+      const componentName = route.component
+      route.component = require(`../widgets/pages/${route.component}/p-layout.vue`).default
+      if (route.children) {
+        route.children.map((child) => {
+          child.component = require(`../widgets/pages/${componentName}/${child.component}/m-layout.vue`).default
+          return child
+        })
+      }
     }
     return route
   })
